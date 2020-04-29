8ae040d8b7ca070e10a9f2449076ec3188852f8f
rudiganzi <rodolfoganzy@gmail.com>
2020-04-29 16:53:20 +0100

    file.h

---
+++ serpentino.h
@@ -0,0 +1,14 @@
+
+
+#ifndef SERPENTINO_H
+#define SERPENTINO_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+extern char* serpentino_encode(const char* s);
+
+
+
+#endif // !SERPENTINO_h
